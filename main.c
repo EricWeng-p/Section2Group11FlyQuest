c64b1d9da6b37e1f54c8d8789ae2ce4baa83369b
aggarwalnanu518@gmail.com <aggarwalnanu518@gmail.com>
2022-03-21 14:57:05 -0400

    Save/Load Module

--- main.c
+++ main.c
@@ -1,9 +1,80 @@
-#include <stdio.h>
+// C program to Open a File,
+// Write in it, And Close the File
+#define _CRT_SECURE_NO_WARNINGS
 
-int main(void) {
-// working
-	printf("Hello World! \n");
+# include <stdio.h>
+# include <string.h>
 
-	return 0;
+void filewrite(char fname, char lname, double cardnumber, int cvv)
+{
+	FILE* filePointer;
+
+	filePointer = fopen("Test.txt", "w");
+	if (filePointer == NULL)
+	{
+		printf("Test.txt file failed to open.");
+	}
+	else
+	{
+
+		printf("The file is now opened.\n");
+
+
+		fprintf(filePointer, "%c \n", fname);
+		fprintf(filePointer, "%c \n", lname);
+		fprintf(filePointer, "%lf \n", cardnumber);
+		fprintf(filePointer, "%d \n", cvv);
+
+
+		fclose(filePointer);
+
+		printf("Data successfully written in file Test.txt\n");
+		printf("The file is now closed.");
+	}
 }
-//test commit
+
+void fileread(char fname, char lname, double cardnumber, int cvv)
+{
+
+	char firstname = { 0 };
+
+	FILE* filePointer;
+
+	filePointer = fopen("Test.txt", "r");
+
+	if (filePointer == NULL)
+	{
+		printf("Test.txt file failed to open.");
+	}
+	else
+	{
+		printf("The file is now opened.\n");
+
+		fscanf(filePointer, "%c %c %lf %d", &fname, &lname, &cardnumber, &cvv);
+		printf("%c \n", fname);
+		printf("%c \n", lname);
+		printf("%lf \n", cardnumber);
+		printf("%d \n", cvv);
+
+		fclose(filePointer);
+
+		printf("Data successfully read from file Test.txt\n");
+		printf("The file is now closed.");
+	}
+}
+
+
+
+
+
+void main()
+{
+	char fname = 'S';
+	char lname = 'A';
+	double cardnumber = 17;
+	int cvv = 101;
+
+	filewrite(fname, lname, cardnumber, cvv);
+	fileread(fname, lname, cardnumber, cvv);
+
+}
