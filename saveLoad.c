beb07c25586b1cc02270708fe6fe66a8805427ee
Douped <44848217+Douped@users.noreply.github.com>
2022-03-31 15:15:00 -0400

    defined remaining functions in header, added code for loadUserListFromFile & printUserList functions

--- saveLoad.c
+++ saveLoad.c
@@ -1,6 +1,20 @@
 #include"saveLoad.h"
+//list size functions
+int getUserListSize() {
+	return userListSize;
+}
+int getFlightListSize() {
+	return flightListSize;
+}
+void setUserListSize(int s) {
+	userListSize = s;
+}
+void setFlightListSize(int s) {
+	flightListSize = s;
+}
 
-USER* readUserFile() {
+//user list functions
+USER* loadUserListFromFile() {
 	//open file
 	FILE* fp = fopen("text.txt", "r");
 	//find size of file
@@ -13,12 +27,64 @@ USER* readUserFile() {
 		}
 	}
 	size = size / 4;
-	printf("%d", size);
+	setUserListSize(size);
 	//malloc space
 	USER* userArray = (USER*)malloc(sizeof(USER) * size);
-	//read file line by line into userArray
-
-
+	//read file line by line into tempArray
+	fseek(fp, 0, SEEK_SET);
+	char buffer[NAMESIZE];
+	char count = 1, userCount = -1;
+	while (fgets(buffer, 25, fp)) {
+		//	printf("%s", buffer); //testing
+		switch (count) {
+		case 1:
+			userCount++;
+			strncpy((userArray + userCount)->firstName, buffer, NAMESIZE);
+			count++;
+			break;
+		case 2:
+			strncpy((userArray + userCount)->lastName, buffer, NAMESIZE);
+			count++;
+			break;
+		case 3:
+			(userArray + userCount)->cardNumber = atoi(buffer);
+			count++;
+			break;
+		case 4:
+			(userArray + userCount)->cvv = atoi(buffer);
+			count = 1;
+			break;
+		default:
+			count++;
+			break;
+		}
+	}
 	//close file
 	fclose(fp);
+	//return array ptr
+	return userArray;
+}
+
+void saveUserListToFile(USER* u) {
+
+}
+
+void printUserList(USER* u) {
+	int count = 0;
+	for (int i = 0; i < getSize(); i++) {
+		printf("%s%s%f\n%d\n", (u + count)->firstName, (u + count)->lastName, (u + count)->cardNumber, (u + count)->cvv); //%.0f causes program timeout
+		count++;
+	}
+}
+
+//flight list functions
+FLIGHT* loadFlightListFromFile() {
+	FLIGHT* f;
+	return f;
+}
+void saveFlightListToFile(FLIGHT* f) {
+
+}
+void printFlightList(FLIGHT* f) {
+
 }
--- saveLoad.h
+++ saveLoad.h
@@ -1,3 +1,16 @@
 #pragma once
 #include"dataProcessing.h"
-USER* readUserFile();
+//listsize
+static int userListSize, flightListSize;
+void setUserListSize(int s);
+int getUserListSize();
+void setFlightListSize(int s);
+int getFlightListSize();
+//user
+USER* loadUserListFromFile();
+void saveUserListToFile(USER* u);
+void printUserList(USER* u);
+//flight
+FLIGHT* loadFlightListFromFile();
+void saveFlightListToFile(FLIGHT* f);
+void printFlightList(FLIGHT* f);
