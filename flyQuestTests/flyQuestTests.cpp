cf7550ad55b09dfdad162c586af286b0daa406e0
Eric Weng <Eweng3376@conestogac.on.ca>
2022-04-02 18:11:48 -0400

    Added save/load to file test cases in main.c

--- flyQuestTests/flyQuestTests.cpp
+++ flyQuestTests/flyQuestTests.cpp
@@ -9,13 +9,52 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace flyQuestTests
 {
-	TEST_CLASS(flyQuestTests)
+	TEST_CLASS(SaveFileModuleTests)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		/* Can't test save to file in unit test suite?
+		TEST_METHOD(saveUserListToFile)
 		{
-		
+			USER userlist[2];
+			userlist[0] = generateUser("John", "Doe", 12345678, 321);
+			userlist[1] = generateUser("Jane", "Doe", 87654321, 123);
+
+			saveUserListToFile(userlist);
+			printUserList(userlist);
+
 		}
+
+		TEST_METHOD(saveFlightListToFile)
+		{
+			FLIGHT *flightlist[2];
+			flightlist[0] = generateFlight("London", true);
+			flightlist[1] = generateFlight("Paris", false);
+
+			saveFlightListToFile(flightlist);
+			printFlightList(flightlist);
+		}
+		
+		*/
+		TEST_METHOD(loadUserListFromFile)
+		{
+			loadUserListFromFile();
+		}
+	
+		TEST_METHOD(loadFlightListFromFile)
+		{
+			loadFlightListFromFile();
+		}
+
+		/*TEST_METHOD(saveTicketListToFile)
+		{
+
+		}
+
+		TEST_METHOD(loadTicketListFromFile)
+		{
+
+		}
+		*/
+		
 	};
 }
--- main.c
+++ main.c
@@ -9,5 +9,20 @@ int main(int argc, char* argv[])
 	else
 		printf("no argument given\n");
 
-	return 1;
+
+	FLIGHT* flightlist[2];
+	flightlist[0] = generateFlight("London", true);
+	flightlist[1] = generateFlight("Paris", false);
+
+	saveFlightListToFile(flightlist);
+	printFlightList(flightlist);
+	loadFlightListFromFile();
+
+	USER userlist[2];
+	userlist[0] = generateUser("John", "Doe", 12345678, 321);
+	userlist[1] = generateUser("Jane", "Doe", 87654321, 123);
+
+	saveUserListToFile(userlist);
+	printUserList(userlist);
+	loadUserListFromFile();
 }
